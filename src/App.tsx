a78ed41ddf870ee67723534b91bed9e93b5e8da0
Frans Allen <fransallen@users.noreply.github.com>
2026-01-27 00:06:50 +0700

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/dns-profile-generator">
+      <BrowserRouter basename="/dns-profile-generator/">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
